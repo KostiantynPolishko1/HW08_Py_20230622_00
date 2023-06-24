fa96e030b296129f91c0c152459f7ac00754303c
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-06-24 21:40:23 +0300

    8 Done logic in addition for type list in order to work with section of menu as "2 add contacts"

--- check.py
+++ check.py
@@ -19,3 +19,26 @@
 # print(d1)
 # d1["o"] = d1.pop(10,"x")
 # print(d1)
+
+#Data
+# contacts = {"Kostiantyn": {"mob": "+38-073-340-90-97", "home": "+38-056-790-53-45", "work": "+38-044-750-23-40"},
+#             "Margarita": ["+38-096-835-60-27", "+38-096-440-19-46"],
+#             "Dariya": "+38-066-645-98-81"
+# }
+# arr = ["+38-096-835-60-27", "+38-096-440-19-46"]
+# # print(type(contacts))
+# print(type(arr))
+#
+# for name in contacts:
+#     if type(contacts[name]) == dict:
+#         print(name, ":")
+#         for ph in contacts[name]:
+#             print("\t\t", ph, " -> ", contacts[name][ph])
+#     elif type(contacts[name]) == list:
+#         print(name, ":")
+#         for ph in contacts[name]:
+#             print("\t\t phone -> ", ph)
+#     else:
+#         print(name, " : ", contacts[name])
+
+
