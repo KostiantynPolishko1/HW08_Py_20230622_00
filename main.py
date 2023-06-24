efaa892950ac814c98fa60f75ad798e5fce48182
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-06-24 19:20:52 +0300

    6 Done logic in addition for type list, dict inside dict in order to work with section of menu as "1 show contacts", "4 modify contacts", "5 search contacts"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 #Data
-contacts = {"Kostiantyn": "+38-073-340-90-97",
-            "Margarita": "+38-096-835-60-27",
+contacts = {"Kostiantyn": {"mob": "+38-073-340-90-97", "home": "+38-056-790-53-45", "work": "+38-044-750-23-40"},
+            "Margarita": ["+38-096-835-60-27", "+38-096-440-19-46"],
             "Dariya": "+38-066-645-98-81"
 }
 
@@ -35,7 +35,18 @@ while True:
         else:
             print(":")
             for name in contacts:
-                print(name + ",\tmob: " + contacts[name])
+                if type(contacts[name]) == dict:
+                    print(name, ":")
+                    for ph in contacts[name]:
+                        print("\t\t", ph, " -> ", contacts[name][ph])
+                elif type(contacts[name]) == list:
+                    ind = 0
+                    print(name, ":")
+                    for ph in contacts[name]:
+                        ind += 1
+                        print("\t\t phone {} -> {}".format(ind, ph))
+                else:
+                    print(name, " : ", contacts[name])
 
     elif n == 2:
         print("\nadd new contacts:")
@@ -109,11 +120,49 @@ while True:
     elif n == 4:
         print("\nmodify contacts")
         while True:
+            ph_pos = 0
             name_search = input("\tenter name: ")
             if contacts.get(name_search, False):
-                print("name {} : phone {}".format(name_search, contacts[name_search]))
+                if type(contacts[name_search]) == dict:
+                    print(name_search, ":")
+                    for ph in contacts[name_search]:
+                        print("\t\t", ph, " -> ", contacts[name_search][ph])
+                elif type(contacts[name_search]) == list:
+                    print(name_search, ":")
+                    for ph in contacts[name_search]:
+                        ph_pos += 1
+                        print("\t\t phone {} -> {}".format(ph_pos, ph))
+                else:
+                    print("name {} : phone {}".format(name_search, contacts[name_search]))
+
                 name_modify = input("\n\tenter new name\t-> ")
-                contacts[name_modify] = contacts.pop(name_search)
+                contacts[name_modify] = contacts.pop(name_search) # new key
+                print("\tNAME MODIFIED!")
+
+                # select type of phone from dict or phone number from the list
+                ph_type: str = ""
+                ph_num: int = 0
+                if type(contacts[name_modify]) == dict:
+                    while True:
+                        ph_type = input("enter type phone -> ")
+                        if contacts[name_modify].get(ph_type, False):
+                            print("MODIFY! ->", end = '')
+                            print(ph_type, " : ", contacts[name_modify][ph_type])
+                            break
+                        else:
+                            print("enter correct type as:")
+                            for i in contacts[name_modify]:
+                                print("\t\t\t", i)
+                            continue
+                elif type(contacts[name_modify]) == list:
+                    while True:
+                        ph_num = int(input("enter phone Pos. modify -> "))
+                        if ph_num > ph_pos:
+                            print("\nN phone is out of range")
+                            continue
+                        ph_num -= 1
+                        print("MODIFY! -> ph: ", contacts[name_modify][ph_num])
+                        break
 
                 while True:  # check if number in digit format
                     logic = False
@@ -134,10 +183,17 @@ while True:
                             if mobil_modify[len(mobil_modify)-1] == '-':
                                 temp = mobil_modify[0:len(mobil_modify)-1]
                                 mobil_modify = temp
-                            contacts[name_modify] = mobil_modify
+                            print("\tPHONE MODIFIED!")
+
                     if logic:
                         continue
                     else:
+                        if type(contacts[name_modify]) == dict:
+                            contacts[name_modify][ph_type] = mobil_modify
+                        elif type(contacts[name_modify]) == list:
+                            contacts[name_modify][ph_num] = mobil_modify
+                        else:
+                            contacts[name_modify] = mobil_modify
                         break
             else:
                 print("\"{}\" is absent in contacts".format(name_search))
@@ -155,7 +211,16 @@ while True:
         while True:
             name_search = input("\tenter name: ")
             if contacts.get(name_search, False):
-                print("name {} : phone {}".format(name_search, contacts[name_search]))
+                if type(contacts[name_search]) == dict:
+                    print(name_search, ":")
+                    for ph in contacts[name_search]:
+                        print("\t\t", ph, " -> ", contacts[name_search][ph])
+                elif type(contacts[name_search]) == list:
+                    print(name_search, ":")
+                    for ph in contacts[name_search]:
+                        print("\t\t phone -> ", ph)
+                else:
+                    print("name {} : phone {}".format(name_search, contacts[name_search]))
             else:
                 print("\"{}\" is absent in contacts".format(name_search))
                 continue
